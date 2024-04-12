afddef2daf14eb155a9f5be39d7bd2b17a83e944
francek prijatelj <prijatelj.francek@gmail.com>
2024-04-12 20:55:20 +0200

    WIP

--- extensions/bun/run.cmd
+++ extensions/bun/run.cmd
@@ -1,7 +1,7 @@
-set BUN_INSTALL="%1\extensions\bun\_runtime"
+set BUN_EXE=%1\\extensions\\bun\\_runtime\\bun.exe
 
-if exist "%1\extensions\bun\main.js" (
-    %BUN_INSTALL%\bin\bun run --inspect "%1\extensions\bun\main.js" %2=%3 %4=%5 %5=%6
+if exist %1\\extensions\\bun\\main.js (
+    %BUN_EXE% run --inspect %1\\extensions\\bun\\main.js %2=%3 %4=%5 %5=%6
 ) else (
-    "%1\extensions\bun\main-app.exe" %2=%3 %4=%5 %5=%6
+    %1\\extensions\\bun\\main-app.exe %2=%3 %4=%5 %5=%6
 )
